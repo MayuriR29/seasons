238942487043c69edeacb805e2eb87b4d638b557
Mayuri Rathod <mayuri.gr@gmail.com>
2019-02-21 11:42:14 +0800

    removed test file

--- src/index.test.js
+++
@@ -1,9 +0,0 @@
-import { shallow } from "enzyme";
-import { SeasonDisplay } from "./components/SeasonDisplay";
-
-describe("Test SeasonDisplay", () => {
-  it("renders correctly", () => {
-    const wrapper = shallow(<SeasonDisplay />);
-    expect(wrapper).toMatchSnapshot();
-  });
-});
