72c6a0f7a25ef22790c2108a525f1f50feddd890
Mayuri Rathod <mayuri.gr@gmail.com>
2019-02-21 11:18:23 +0800

    added jest and enzyme

--- src/index.js
+++ src/index.js
@@ -1,35 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-
-class App extends React.Component{
-    constructor(props){
-       super(props); 
-       this.state={lat:null, errorMessage:''}
-       window.navigator.geolocation.getCurrentPosition(
-        position =>{
-            this.setState({
-                lat:position.coords.latitude
-            })
-        },//success call back
-        err =>{
-            this.setState({
-                errorMessage:err.message
-            })
-        }
-      );
-    }
-    render(){
-        
-
-      return(
-          <div>
-              Latitude:{this.state.lat}
-              <br/>
-              Error:{this.state.errorMessage}
-              </div>
-      )  
-    }
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { lat: null, errorMessage: "" };
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          lat: position.coords.latitude
+        });
+      }, //success call back
+      err => {
+        this.setState({
+          errorMessage: err.message
+        });
+      }
+    );
+  }
+  render() {
+    return (
+      <div>
+        Latitude:{this.state.lat}
+        <br />
+        Error:{this.state.errorMessage}
+      </div>
+    );
+  }
 }
 
-ReactDOM.render(<App />,document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
